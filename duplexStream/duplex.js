dabc2c88ef15fb0c3fb402b3003a9122c0536320
Rohan Chougule <rohan183chougule@gmail.com>
2018-09-25 10:36:15 +0530

    duplex stream try

---
+++ duplexStream/duplex.js
@@ -0,0 +1,26 @@
+/**
+ * Duplex object which does the work of both readable and writable stream.
+ * As it it implements both the Readable and Writable interfaces
+ */
+
+const { Duplex } = require('stream');
+
+const dupStream = new Duplex({
+    write(chunk, encoding, callback) {
+        console.log(chunk.toString());
+        callback();
+    },
+
+    read(size) {
+        this.push(String.fromCharCode(this.currentCharCode++))
+        if(this.currentCharCode > 90)
+            this.push(null);
+    }
+
+})
+
+dupStream.currentCharCode = 65;
+
+process.stdin.pipe(dupStream).pipe(process.stdout);
+
+dupStream.push('hello bitches');
